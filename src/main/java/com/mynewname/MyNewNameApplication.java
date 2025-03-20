b586fa72bcb3e232c55e37d33d7ee28ccde72777
KANG JISEOK <rkdwltjr5876@naver.com>
2025-03-20 11:12:46 +0900

    [init]

---
+++ src/main/java/com/mynewname/MyNewNameApplication.java
@@ -0,0 +1,13 @@
+package com.mynewname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNewNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mynewname/MyNewNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynewname;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
