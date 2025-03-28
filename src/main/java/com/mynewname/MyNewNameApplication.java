54b105b827f86cd8545bf1a921be8c62d9218c7a
KANG JISEOK <rkdwltjr5876@naver.com>
2025-03-28 14:47:10 +0900

    CICD test29

--- src/main/java/com/mynewname/MyNewNameApplication.java
+++ src/main/java/com/mynewname/MyNewNameApplication.java
@@ -3,7 +3,9 @@ package com.mynewname;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	org.springframework.ai.autoconfigure.vectorstore.milvus.MilvusVectorStoreAutoConfiguration.class
+})
 public class MyNewNameApplication {
 
 	public static void main(String[] args) {
