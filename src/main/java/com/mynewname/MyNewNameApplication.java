1b3305ccb1cdf813429d70067b6373ed6ac30b05
KANG JISEOK <rkdwltjr5876@naver.com>
2025-03-28 14:51:59 +0900

    CICD test30 milvus 의존성 삭제

--- src/main/java/com/mynewname/MyNewNameApplication.java
+++ src/main/java/com/mynewname/MyNewNameApplication.java
@@ -3,9 +3,7 @@ package com.mynewname;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-	org.springframework.ai.autoconfigure.vectorstore.milvus.MilvusVectorStoreAutoConfiguration.class
-})
+@SpringBootApplication
 public class MyNewNameApplication {
 
 	public static void main(String[] args) {
